051aa6f0a2d9bae17b3e03f3e579c688a1c1576a
Corey Butler <corey@coreybutler.com>
2011-09-06 16:23:51 -0500

    Simpler IP address

--- slides/slide-server.js
+++ slides/slide-server.js
@@ -86,3 +86,67 @@ app.get('*', function(req, res) {
     res.send(buffer.toString());
   });
 });
+
+
+//Presentation Specific Code
+app.get('/server', function(req,res) {
+	getNetworkIP(function(error, ip){
+		if (error) console.log('error:',error);
+		
+		res.end(ip);
+	}, false);
+});
+
+var getNetworkIP = (function () {
+    var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
+
+    var exec = require('child_process').exec;
+    var cached;    
+    var command;
+    var filterRE;
+
+    switch (process.platform) {
+    // TODO: implement for OSs without ifconfig command
+    case 'darwin':
+         command = 'ifconfig';
+         filterRE = /\binet\s+([^\s]+)/g;
+         // filterRE = /\binet6\s+([^\s]+)/g; // IPv6
+         break;
+    default:
+         command = 'ifconfig';
+         filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+         // filterRE = /\binet6[^:]+:\s*([^\s]+)/g; // IPv6
+         break;
+    }
+
+    return function (callback, bypassCache) {
+         // get cached value
+        if (cached && !bypassCache) {
+            callback(null, cached);
+            return;
+        }
+        // system call
+        exec(command, function (error, stdout, sterr) {
+            var ips = [];
+            // extract IPs
+            var matches = stdout.match(filterRE);
+            // JS has no lookbehind REs, so we need a trick
+            for (var i = 0; i < matches.length; i++) {
+                ips.push(matches[i].replace(filterRE, '$1'));
+            }
+
+            // filter BS
+            for (var i = 0, l = ips.length; i < l; i++) {
+                if (!ignoreRE.test(ips[i])) {
+                    //if (!error) {
+                        cached = ips[i];
+                    //}
+                    callback(error, ips[i]);
+                    return;
+                }
+            }
+            // nothing found
+            callback(error, null);
+        });
+    };
+})();
